aefde166e3474c35a021d1f52ecadf76a71357ee
Mudassir Rehman <13276017+mudassir7446@users.noreply.github.com>
2021-04-08 22:38:54 +0530

    added responsive css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { BreakpointObserver,Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(breakpointOberver:BreakpointObserver){
+    breakpointOberver.observe([Breakpoints.XSmall]).subscribe(result =>{
+      this.orgNameClass =result.matches ? "hide-on-xsmall-device":"";
+    });
+  }
   title = 'Trust Portal';
   orgName = 'Shifa-E-Rehmani';
+  orgNameClass:string ="";
   //TODO make the organization name configurable
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { LayoutModule} from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {MatIconModule} from '@angular/material/icon';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
